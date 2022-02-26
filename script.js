7cd36b40896fca0cb315ea76057e38174bee8969
iamjhp <jooho.park@outlook.com>
2022-02-26 02:04:49 +0100

    added the function for adding a book to the library

--- script.js
+++ script.js
@@ -1,11 +1,25 @@
-const newBookBtn = document.getElementById("btn")
-const addBtn = document.getElementById("add-btn")
+const addBookBtn = document.getElementById("btn")
+const addBookSubmitBtn = document.getElementById("add-btn")
 const popUpForm = document.querySelector(".form-popup")
 
-newBookBtn.addEventListener("click", () => popUpForm.style.display = "block")
-addBtn.addEventListener("click", () => popUpForm.style.display = "none")
+addBookBtn.addEventListener("click", () => popUpForm.style.display = "block")
+addBookSubmitBtn.addEventListener("click", (e) => {
+    if (!checkRequiredFormInputViaDOM()) {
+        // not all required inputs are entered
+        return
+    }
 
-let myLibrary = ["Book1", "Book2"]
+    e.preventDefault() // prevents the page from reloading
+    popUpForm.style.display = "none"
+
+    addBookToLibary();
+}) 
+
+
+//popUpForm.style.display = "none")
+
+let myLibrary = []
+
 
 function Book(title, author, pages, hasRead) {
     this.title = title
@@ -14,13 +28,36 @@ function Book(title, author, pages, hasRead) {
     this.hadRead = hasRead
 }
 
-function addBookToLibary() {
+const book1 = new Book("ABC", "Hans Peter", 25, false)
+const book2 = new Book("DEF", "Thomas Hey", 3000, true)
+myLibrary.push(book1)
+myLibrary.push(book2)
 
+function addBookToLibary() {
+    const newBookTitle = document.getElementById("title").value
+    const newBookAuthor = document.getElementById("author").value
+    const newBookPages = document.getElementById("pages").value
+    const hasRead = document.getElementById("hasRead").checked
+
+    const newBook = new Book(newBookTitle, newBookAuthor, newBookPages, hasRead)
+
+    myLibrary.push(newBook)
+
+    console.log(myLibrary)
 }
 
 function printAllBooks() {
     myLibrary.forEach(book => console.log(book))
 }
 
+// check if all form required inputs have been entered
+function checkRequiredFormInputViaDOM () {
+    let requiredForm = document.querySelector('form').querySelectorAll("[required]")
+    for (let el of requiredForm) {
+        if (!el.reportValidity()) {
+            return false;
+        }
+    }
+    return true;
+}
 
-printAllBooks()
